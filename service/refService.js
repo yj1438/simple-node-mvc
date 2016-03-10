e8d514cdfb92492d729db795cf83cb1742106f97
yinjie <yj1438@163.com>
2016-03-10 12:23:41 +0800

    update

---
+++ service/refService.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Database = require('../common/database');
+const ProtocolMap = {
+    '1': 'http1',
+    '2': 'spdy2',
+    '3': 'spdy3',
+    '3.1': 'spdy3_1',
+    '4': 'http2'
+}; 
+/**
+ * 记录点击率
+ * data: id-id ref-来源 (protocal)-参照ProtocolMap的 key
+ * 通过ON DUPLICATE KEY UPDATE语法，实现一句 SQL 有则加1，无则添加的功用
+ */
+exports.addCTR = function(data, callback) {
+    //INSERT INTO stat_ref(id, ref, http1) values ('babytree', 'babytree', 1) ON DUPLICATE KEY UPDATE http1 = http1 + 1;
+    let id = data.id,
+        ref = data.ref,
+        protocol = ProtocolMap[data.protocol];
+    if (!id || !ref || !protocol) {
+        callback && callback('参数错误', null);
+        return;
+    }
+    let sql = "INSERT INTO stat_ref(id, ref, " + protocol + ") values ('" + id + "', '" + ref + "', 1)"
+         + " ON DUPLICATE KEY UPDATE " + protocol + " = " + protocol + " + 1;";
+    Database.handle(sql, {}, callback || () => {});
+}
