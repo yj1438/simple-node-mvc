66f0d5cd30553f579de88487ecf6ca2984e8509f
yinjie <yj1438@163.com>
2016-08-24 19:31:09 +0800

    update

--- build/common/wechat/AppKey.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var APP_KEY = {
-    APP_ID: 'wxf98a980502b19fc2',
-    APP_SECRET: 'ac1c85f54d1acf9b7de203e6a77b0ba4'
-};
-
-module.exports = APP_KEY;
-//# sourceMappingURL=AppKey.js.map
--- build/controllers/compass.js
+++
@@ -1,9 +0,0 @@
-/**
- * 指南针
- */
-'use strict';
-
-exports.index = function () {
-  this.render('compass/index', {});
-};
-//# sourceMappingURL=compass.js.map
